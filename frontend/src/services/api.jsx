64e2127026f014536f8d21e52ad29e74330f463a
vshivam05 <sv699291@gmail.com>
2025-04-19 19:58:23 +0530

    backend deployed

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/match";
+// const API_URL = "http://localhost:5000/api/match";
+const API_URL = "https://cricbuzz-1-qctn.onrender.com/api/match";
 
 export const initMatch = async (teamA, teamB, players) => {
   const response = await axios.post(`${API_URL}/init`, {
